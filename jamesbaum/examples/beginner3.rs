c9452025b356ce2cf721b438f117c7f10008fdb4
Wilhelm Huber <wilhelm.g.huber@bmw.de>
2021-02-25 09:17:36 +0100

    Add jamesbaum decreasing and increasing pyramids

---
+++ jamesbaum/examples/beginner3.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 2 {
+        if let Ok(largest) = args[1].parse::<usize>() {
+            for i in (1..=largest).rev() {
+                println!("{}", "*".repeat(i));
+            }
+        }
+    }
+}
